8dacf6781d6e7cd506558eea7ce4eeacb20d6280
Abdulkadir Şen <abdulkadir.sen@neyasis.com>
2020-08-27 14:47:49 +0300

    binary search was added

---
+++ src/Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Searching
+{
+    public class BinarySearch
+    {
+        public List<int> Numbers { get; }
+
+        public BinarySearch(List<int> numbers)
+        {
+            Numbers = numbers ?? throw new ArgumentNullException(nameof(numbers));
+        }
+
+        public bool Search(int value)
+        {
+            int max = Numbers.Count - 1;
+            int mid;
+            int min = 0;
+
+            while (min <= max)
+            {
+                mid = (min + max) / 2;
+
+                if (Numbers[mid] == value)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (value < Numbers[mid])
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Searching/BinarySearchTests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Searching;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests.Searching
+{
+    public class BinarySearchTests
+    {
+        [Fact]
+        public void Binary_Search_Searhing_Correctly_Test()
+        {
+            //Arrange
+            List<int> numbersToSearch = new List<int>
+            {
+                5,1,7,3,8,34,2
+            };
+
+            BinarySearch searcher = new BinarySearch(numbersToSearch);
+
+            //Act
+            var existForTrue = searcher.Search(34);
+
+            var existForFalse = searcher.Search(100);
+
+            //Assert
+            Assert.True(existForTrue);
+            Assert.False(existForFalse);
+        }
+    }
+}
