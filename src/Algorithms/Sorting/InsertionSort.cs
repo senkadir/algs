7824ab79543e8b28d08161d001cc7345294dfb4a
Abdulkadir Şen <abdulkadir.sen@neyasis.com>
2020-08-27 12:27:08 +0300

    insertion was added

---
+++ src/Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public class InsertionSort
+    {
+        public List<int> Numbers { get; }
+
+        public InsertionSort(List<int> numbers)
+        {
+            Numbers = numbers ?? throw new ArgumentNullException(nameof(numbers));
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i < Numbers.Count - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (Numbers[j - 1] > Numbers[j])
+                    {
+                        int temp = Numbers[j - 1];
+                        Numbers[j - 1] = Numbers[j];
+                        Numbers[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Sorting/InsertionSortTests.cs
@@ -0,0 +1,33 @@
+﻿using Algorithms.Sorting;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests.Sorting
+{
+    public class InsertionSortTests
+    {
+        [Fact]
+        public void Insertion_Sort_Sorting_Correctly_Test()
+        {
+            //Arrange
+            List<int> numbersToSort = new List<int>
+            {
+                5,1,7,3,8,34,2
+            };
+
+            InsertionSort sorter = new InsertionSort(numbersToSort);
+
+            //Act
+            sorter.Sort();
+
+            //Assert
+            Assert.Equal(1, sorter.Numbers[0]);
+            Assert.Equal(2, sorter.Numbers[1]);
+            Assert.Equal(3, sorter.Numbers[2]);
+            Assert.Equal(5, sorter.Numbers[3]);
+            Assert.Equal(7, sorter.Numbers[4]);
+            Assert.Equal(8, sorter.Numbers[5]);
+            Assert.Equal(34, sorter.Numbers[6]);
+        }
+    }
+}
